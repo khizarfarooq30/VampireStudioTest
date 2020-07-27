53dbab0d4ef68608d8b1f8824b90bd5dc8cb1f2c
KhizarFarooqGameDev <khizarfarooq30@gmail.com>
2020-07-27 18:59:40 +0500

    CharacterSelection Script completed

--- Test Scene/Assets/Scripts/CharacterSelection.cs
+++ Test Scene/Assets/Scripts/CharacterSelection.cs
@@ -4,8 +4,45 @@ using UnityEngine;
 
 public class CharacterSelection : MonoBehaviour
 {
+    public CharacterSelector[] selectableCharacters;
+
+    private GameObject currentCharacter;
+    private int characterIndex;
+
+    private void Start()
+    {
+        SwitchCharacters();
+    }
+
+    private void SwitchCharacters()
+    {
+        Destroy(currentCharacter, 0f);
+        currentCharacter = Instantiate(selectableCharacters[characterIndex].character, transform.position, transform.rotation, transform);
+    }
+
+    public void LeftButton()
+    {
+        characterIndex--;
+        if(characterIndex < 0)
+        {
+            characterIndex = selectableCharacters.Length - 1;
+        }
+        SwitchCharacters();
+    }
+
+    public void RightButton()
+    {
+        characterIndex++;
+        if (characterIndex == selectableCharacters.Length)
+        {
+            characterIndex = 0;
+        }
+        SwitchCharacters();
+    }
+
     [System.Serializable]
-    public class CharacterSelector{
-        public Sprite[] characters;
+    public class CharacterSelector
+    {
+        public GameObject character;
     }
 }
