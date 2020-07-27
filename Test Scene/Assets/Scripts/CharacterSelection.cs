bfe7285886dd768a73ea8ea2b19a9e40ca08eda0
KhizarFarooqGameDev <khizarfarooq30@gmail.com>
2020-07-27 18:06:25 +0500

    2nd commit

---
+++ Test Scene/Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [System.Serializable]
+    public class CharacterSelector{
+        public Sprite[] characters;
+    }
+}
---
+++ Test Scene/Assets/Scripts/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    private int currentSceneIndex;
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadNextScene()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
